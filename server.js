15cabca0bd5d50d143c13198e60ae1517ab0cceb
DobrinKazandziev <dobrinkazandziev94@gmail.com>
2019-06-27 15:59:56 +0200

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image.js');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-tapered-56882',
-      user : 'dobrink',
-      password : 'password',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl:true
     }
 });
 
